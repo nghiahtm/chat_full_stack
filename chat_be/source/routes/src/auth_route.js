f30a6cb59a300382657e06d04fe3193a499f1cfd
DESKTOP-JTKI6JE\ADMIN <nghiahtm97@gmail.com>
2026-04-05 18:18:58 +0700

    refresh token

--- chat_be/source/routes/src/auth_route.js
+++ chat_be/source/routes/src/auth_route.js
@@ -3,6 +3,8 @@ import { AuthController } from "../../controllers/controller.js";
 import userValidator from "../../configs/validators/create_user_validator.js";
 import authValidator from "../../configs/validators/user_login.js";
 import * as ValidatorMiddleware from "../../configs/core/valid_middleware.js";
+import authMiddleware from "../../middlewares/auth_middleware.js";
+
 const router = express.Router();
 
 router.post(
@@ -17,5 +19,6 @@ router.post(
 );
 
 router.post("/logout", AuthController.logout);
+router.post("/refresh_token", AuthController.refreshAccToken);
 
 export default router;
